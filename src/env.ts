24685835ecfe36b31881e41d8edb122d14abea2c
Raphael Berger <raphaelmb@gmail.com>
2025-02-18 19:45:51 -0300

    new: add database connection + subscription table

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,11 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  POSTGRES_URL: z.string().url(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  REDIS_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
